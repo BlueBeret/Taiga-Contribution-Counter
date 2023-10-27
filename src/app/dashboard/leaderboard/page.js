6f56018a4691728c75505137952c325e3906eae2
Blueberet <me@lurifos.dev>
2023-10-27 15:07:26 +0700

    feat: update leaderboard layout

--- src/app/dashboard/leaderboard/page.js
+++ src/app/dashboard/leaderboard/page.js
@@ -58,8 +58,8 @@ export default function Leaderboard() {
                 setLeaderboard(data);
             });
     }, [user]);
-    return <div className="w-full h-full p-8 flex gap-8 items-start overflow-clip">
-        <div className="rank-cards-container no-scroll w-1/3 h-[100%] flex flex-col overflow-auto gap-4">
+    return <div className="w-full h-full p-8 flex gap-8 items-start justify-center overflow-clip">
+        <div className="rank-cards-container no-scroll w-full lg:w-1/3 h-[100%] flex flex-col items-center overflow-auto gap-4">
             <div className="flex flex-col gap-0">
                 <span className="text-pink-0">
                     {thisMonthName} {thisYear} Nyan Nyan warrios
@@ -72,11 +72,12 @@ export default function Leaderboard() {
                 let rank = index + 1
                 // point 2 decimal places
                 let point = user.point.toFixed(2)
+                if (point <= 0) return ""
                 return <div key={index} className={`w-full max-w-[500px] relative border flex items-center px-8 py-[18px] gap-4 ${rank > 3 ? "border-orange-0" : "border-yellow-0"}`}>
                     <img src={user.image} height={40} width={40} className="rounded-full"></img>
                     <span className="w-1/4 break-words">{user.username}</span>
-                    <div className="flex w-full justify-end">
-                        <div className="flex flex-col 2xl:px-4">
+                    <div className="flex w-full gap-2">
+                        <div className="flex flex-col ml-auto 2xl:px-4">
                             <span className="text-[12px]">Point</span>
                             <span className="text-[32px] leading-[48px]">{point}</span>
                         </div>
@@ -100,7 +101,7 @@ const Graph = ({ users }) => {
 
     const maxPoint = users[0].point
 
-    return <div className="flex flex-col items-center justify-center overflow-hidden w-2/3 h-full">
+    return <div className="hidden lg:flex flex-col items-center justify-center overflow-hidden w-2/3 h-full">
         <h1 className="text-pink-0 text-[32px] leading-[48px]">Cat'O Meter</h1>
         <div className="flex items-end h-full w-full gap-4">
             {users.map((user, index) => {
